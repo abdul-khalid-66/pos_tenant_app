8fbe702d6271a209d717d4d0128e77f0ff680f22
AK Project <abdulkhalidmasood66@gmail.com>
2025-06-30 23:09:32 +0500

    added x-tenant-app-layout to category customer product and sales files

--- resources/views/admin/sales/edit.blade.php
+++ resources/views/admin/sales/edit.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-tenant-app-layout>
     @push('css')
     <link rel="stylesheet" href="{{ asset('backend/assets/css/backend-plugin.min.css') }}">
     <link rel="stylesheet" href="{{ asset('backend/assets/css/backend.css?v=1.0.0') }}">
@@ -368,4 +368,4 @@
     });
     </script>
     @endpush
-</x-app-layout>
+</x-tenant-app-layout>
